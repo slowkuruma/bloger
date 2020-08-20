7b4dc623bbd82a58649ae0f60bec2d857deb89b5
slowkuruma <stevan.han82@gmail.com>
2020-08-20 10:01:25 -0700

    basic server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+
+
+
+
+
+
+const port = 3000
+
+app.listen(port, function () {
+    console.log("Express Train... CHOO!!! CHOO!!! Port ${ port }")
+})
