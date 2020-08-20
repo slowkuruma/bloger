8925956ea48a7cf8fa7465583f4aa949e040a35f
slowkuruma <stevan.han82@gmail.com>
2020-08-20 13:36:07 -0700

    updated

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 
 
+require("dotenv").config()
+require("./config/database")
 
 
-
-
+app.use(express.urlencoded({ extended: false }))
 
 const port = 3000
 
