1d95361dea8562a73214d63e8063395c7a95c7ff
slowkuruma <stevan.han82@gmail.com>
2020-08-21 18:43:18 -0700

    server.js adjustments

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
-const express = require("express")
-const app = express()
-const path = require("path")
-const cors = require("cors")
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const logger = require("morgan");
+const favicon = require("serve-favicon");
 
-const blogsRoutes = require("./routes/blogs-routes")
-const usersRoutes = require("./routes/users-routes")
+const blogsRoutes = require("./routes/blogs-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
+const app = express();
 
-require("dotenv").config()
-require("./config/database")
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+require("dotenv").config();
+require("./config/database");
+
+app.use(logger("dev"));
+app.use(express.json());
+
+app.use(favicon(path.join(_dirname, "build", "favicon.ico")));
+app.use(express.static(path.join(_dirname, "build")));
 
 
-app.use(express.urlencoded({ extended: false }))
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requessted-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
+});
 
-app.use(methodOverride('_method'))
+
+
+app.use("api/blogs", blogsRoutes);
+app.use("api/users", usersRoutes);
 
 
 
 
-
-
-app.use("/blogs", blogsRoutes);
-app.use("/users", usersRoutes);
 
 
 
