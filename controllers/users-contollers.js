4ce074ef6dc3273ff9d891fdcae0647c2270beec
slowkuruma <stevan.han82@gmail.com>
2020-08-21 19:07:37 -0700

    bug fix

--- controllers/users-contollers.js
+++ controllers/users-contollers.js
@@ -3,12 +3,12 @@ const HttpError = require("../models/http-error");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const user = require("../models/user");
+
 
 const getUsers = async (req, res, next) => {
     let users;
     try {
-        users = await User.find({}, "-passowrd");
+        users = await User.find({}, "-password");
     } catch (err) {
         const error = new HttpError("Fetching users failed, try again later", 500);
         return next(error);
