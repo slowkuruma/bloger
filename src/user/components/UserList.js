73a065eddef347f6da9add858884c656dae03f9c
slowkuruma <stevan.han82@gmail.com>
2020-08-22 17:53:46 -0700

    User/components

---
+++ src/user/components/UserList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./UsersList.css";
+import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
+
+const UsersList = (props) => {
+    if (props.items.length === 0) {
+        return (
+            <div className="center">
+                <Card>
+                    <h2>There are currently no users.</h2>
+                </Card>
+            </div>
+        );
+    }
+
+    return (
+        <ul className="users-list">
+            {props.items.map((user) => (
+                <UserItem
+                    key={user.id}
+                    id={user.id}
+                    image={user.image}
+                    name={user.name}
+                    twottsCount={user.blogs.length}
+                />
+            ))}
+        </ul>
+    );
+};
+
+export default UsersList;
