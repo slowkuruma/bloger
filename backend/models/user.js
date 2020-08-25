26a268a31eae720d4dce52aed9175c6e873b3d93
slowkuruma <stevan.han82@gmail.com>
2020-08-24 21:20:15 -0700

    re-added folders/files

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,9 +6,9 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, minlength: 8 },
+    password: { type: String, required: true, minlength: 5 },
     image: { type: String, required: true },
-    twotts: [{ type: mongoose.Types.ObjectId, required: true, ref: "Blog" }],
+    blogs: [{ type: mongoose.Types.ObjectId, required: true, ref: "Blog" }],
 });
 
 userSchema.plugin(uniqueValidator);
